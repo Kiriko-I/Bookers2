b1db8138a90910f0897d18cbfdcf08b4ce386ffc
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-03-07 13:01:06 +0000

    [Add]files required for the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
 end
