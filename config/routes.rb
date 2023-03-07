a43cad3201308b840e0499b642839d2c81a617be
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-03-07 23:12:12 +0000

    [Add]Bootstrap, routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'users/show'
   get 'users/edit'
-  get 'books/new'
   post 'books' => 'books#create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books, only: [:new, :index, :show, :edit]
+  # books newは不要?の予定
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
