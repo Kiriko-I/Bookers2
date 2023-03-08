1e82b550cc73b60341ce704014ff9f0b68ba2571
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-03-08 04:55:22 +0000

    [Add]viewfile, routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def index
+    @books = Book.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  post 'books' => 'books#create'
-  resources :books, only: [:new, :index, :show, :edit]
-  # books newは不要?の予定
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :show, :edit]
+  # books newは不要?の予定
+  post 'books' => 'books#create'
+  get '/books' => 'books#index'
 end
+
+# routing順番大切！
