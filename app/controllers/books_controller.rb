b31f1a30be8b180cc7a75b16cf9f1ffb95b279e6
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-03-09 00:52:13 +0000

    [Add]user's view, controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,16 @@
 class BooksController < ApplicationController
   def new
-    
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to new_book_path
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -14,7 +18,12 @@ class BooksController < ApplicationController
 
   def destroy
   end
-  
+
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def index
     @books = Book.all
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
