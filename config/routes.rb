6fd11c083f0bfee08ac282a330dbef903ce7c2bc
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-03-14 10:26:50 +0000

    [Finish]4-27

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'home/about' => 'homes#about'
+  get 'home/about' => 'homes#about', as: 'about'
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :new, :create, :edit, :destroy]
   patch 'users/:id' => 'users#update', as: 'update_user'
